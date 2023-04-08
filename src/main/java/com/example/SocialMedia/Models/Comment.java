a3ef8460f31a0110fb2e6e7098b66a2d072b8166
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2023-04-08 08:27:45 +0530

    added getallposts

---
+++ src/main/java/com/example/SocialMedia/Models/Comment.java
@@ -0,0 +1,6 @@
+package com.example.SocialMedia.Models;
+
+public class Comment {
+	public String username;
+	public String comment;
+}
--- src/main/java/com/example/SocialMedia/Repositories/PostsRepository.java
+++ src/main/java/com/example/SocialMedia/Repositories/PostsRepository.java
@@ -1,12 +1,13 @@
-package com.example.SocialMedia;
+package com.example.SocialMedia.Repositories;
 
-import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.example.SocialMedia.Models.Post;
+
 @Repository
-public interface PostsRepository extends MongoRepository<Post, ObjectId> {
+public interface PostsRepository extends MongoRepository<Post, String> {
 	
 	@Query("{username: '?0'}")
 	public Post findPostByUser(String username);
