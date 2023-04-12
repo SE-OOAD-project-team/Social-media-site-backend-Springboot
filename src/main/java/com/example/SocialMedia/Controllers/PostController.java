c5df994e28d759a0f01830d4364a9916bb2e9c77
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2023-04-12 13:45:54 +0530

    get and create posts

--- src/main/java/com/example/SocialMedia/Controllers/PostController.java
+++ src/main/java/com/example/SocialMedia/Controllers/PostController.java
@@ -1,20 +1,52 @@
 package com.example.SocialMedia.Controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.SocialMedia.Models.Comment;
+import com.example.SocialMedia.Models.Post;
 import com.example.SocialMedia.Repositories.PostsRepository;
 
 @RestController
-@RequestMapping("/api/v2")
+@RequestMapping("/api/v2/post")
 public class PostController {
 	@Autowired
 	PostsRepository postsRepo;
+
+	@GetMapping("get/{id}")
+	public ResponseEntity<Post> getPost(@PathVariable("id") String id) {
+		Optional<Post> post = postsRepo.findById(id);
+
+		if (post.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(post.get());
+		}
+	}
+
+	@PostMapping(value = "create", consumes = "application/json")
+	public Post createPost(@RequestBody Post post) {
+		if (post.comments == null) {
+			post.comments = new ArrayList<Comment>();
+		}
+		post.comments_count = post.comments.size();
+		post.likes_count = 0;
+
+		Post created = postsRepo.save(post);
+
+		return created;
+	}
 
 	@GetMapping("getallposts")
 	public List<String> getAllPosts() {
