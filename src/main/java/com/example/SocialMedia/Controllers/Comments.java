83a41558125531fad6f419208443dfae42866f05
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2023-04-23 15:45:13 +0530

    Add comments

--- src/main/java/com/example/SocialMedia/Controllers/Comments.java
+++ src/main/java/com/example/SocialMedia/Controllers/Comments.java
@@ -4,9 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,15 +22,32 @@ import com.example.SocialMedia.Repositories.PostsRepository;
 public class Comments {
 	@Autowired
 	PostsRepository postsRepo;
-	
+
 	@GetMapping("get/{post_id}")
 	public ResponseEntity<List<Comment>> getComments(@PathVariable("post_id") String post_id) {
 		Optional<Post> post = postsRepo.findById(post_id);
 
 		if (post.isEmpty()) {
 			return ResponseEntity.notFound().build();
-		} else {	
+		} else {
 			return ResponseEntity.ok(post.get().comments);
+		}
+	}
+
+	@PostMapping("add/{post_id}")
+	public ResponseEntity<Void> addComment(@PathVariable("post_id") String post_id, @RequestBody Comment comment) {
+		Optional<Post> postOpt = postsRepo.findById(post_id);
+
+		if (postOpt.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			Post post = postOpt.get();
+
+			post.comments.add(comment);
+			
+			post = postsRepo.save(post);
+
+			return ResponseEntity.status(HttpStatus.OK).build();
 		}
 	}
 }
