6f1a54046c2158ba998c19c25b6967b5c28981cd
Shifaan <53972679+Shifaan0010@users.noreply.github.com>
2023-04-25 09:01:54 +0530

    Edit profile

--- src/main/java/com/example/SocialMedia/Controllers/Profile.java
+++ src/main/java/com/example/SocialMedia/Controllers/Profile.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,24 @@ public class Profile {
 			return ResponseEntity.notFound().build();
 		} else {
 			return ResponseEntity.ok(userOpt.get());
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<User> editProfile(@RequestBody User editedUser) {
+		Optional<User> userOpt = usersRepo.findByUsername(editedUser.username);
+
+		if (userOpt.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			User user = userOpt.get();
+
+			user.displayName = editedUser.displayName;
+			user.description = editedUser.description;
+
+			user = usersRepo.save(user);
+
+			return ResponseEntity.ok(user);
 		}
 	}
 }
